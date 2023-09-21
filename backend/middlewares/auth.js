e307188d2b1b868d1e24dd6d1e3bcb6bed0275cb
Denis Shesternev <denshesternev@yandex.ru>
2023-09-21 16:41:23 +0800

    совмещение бэк и фронт

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -5,10 +5,10 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer ', '')) {
+  if (!authorization || !authorization.startsWith('Bearer ')) {
     next(new AuthError('Необходима авторизация.'));
   }
-  const token = authorization.replace('Bearer ');
+  const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
